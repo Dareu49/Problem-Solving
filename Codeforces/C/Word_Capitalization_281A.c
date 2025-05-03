4b25aa6da3ff52359cfcc2e3f7142c3475b42f8d
Dareu49 <bishalsadi49@gmail.com>
2025-05-04 03:18:21 +0600

    Solve 281A - Word Capitalization [implementation, strings]

--- Codeforces/C/Word_Capitalization_281A.c
+++ Codeforces/C/Word_Capitalization_281A.c
@@ -1,5 +1,13 @@
+// 04 May 2025, 03:13 am
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(){
+    char a [1001];
+    scanf("%s",a);
+    a[0] = toupper(a[0]);
+    printf("%s",a);
+
 return 0;
 }
