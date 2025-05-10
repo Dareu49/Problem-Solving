22e664b117e3d9975cb6211df43188c8288e242a
Dareu49 <bishalsadi49@gmail.com>
2025-05-10 10:42:26 +0600

    Solve 110A - Nearly Lucky Number [implementation]

---
+++ Codeforces/C/Nearly_Lucky_Number_110A.c
@@ -0,0 +1,26 @@
+// Started: 09 May 2025, 09:30 pm
+// Solved: 10 May 2025, 10:39 am
+#include <stdio.h> //scanf(), printf()
+#include <string.h>
+
+int main(){
+    int count = 0;
+    long long n;
+    scanf("%lld",&n);
+
+    while(n != 0){
+        if(n % 10 == 4 || n % 10 == 7){
+            count++;
+        }
+            n = n / 10;
+    }
+
+    if(count == 4 || count == 7){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+
+return 0;
+}
